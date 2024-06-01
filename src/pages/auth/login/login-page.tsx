03408cdbeaef121a56a92abecf94992c50120ac1
jeseias <domingosjeseias@gmail.com>
2024-06-01 19:21:40 +0100

    feat: add login form structure

--- src/pages/auth/login/login-page.tsx
+++ src/pages/auth/login/login-page.tsx
@@ -1,7 +1,12 @@
+import { LoginForm } from "./login-form";
+
 export const LoginPage = () => {
   return (
-    <div className="">
-      <h2 className="">Login Page</h2>
+    <div className="grid grid-cols-[1fr_1fr] h-screen w-screen">
+      <div className=""></div>
+      <div className="h-full flex items-center justify-center">
+        <LoginForm />
+      </div>
     </div>
   );
 };
